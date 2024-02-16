8523163357d3292973942a3b66742c3cbced0b5b
Ivo Stoykov <ivostoykov@users.noreply.github.com>
2024-02-16 22:13:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gulp_rename_in_place
+Renames given file types in place without using temporary files or making a copy
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const path = require('path');
+const fs = require('fs');
+
+const baseDir = '/home/ivo/Downloads/';
+const mp3Path = `${baseDir}*.mp3`;
+
+function getNewName(originalName){
+    const newName = originalName
+    .replace(/-{1,}|\,{1,}/g, ' ')
+    .replace(/\.{1,}/g, ' ')
+    .trim()
+    .replace(/\s{1,}/g, '_')
+    .toLowerCase();
+
+    return newName !== originalName ? newName : false;
+}
+
+gulp.task('watch-mp3', function() {
+    return watch(mp3Path, { base: baseDir, events: ['add', 'change'] }, (file) => {
+        // No need for an asynchronous handling or global flag
+        setTimeout(() => {
+            try {
+                const oldPath = path.join(baseDir, file.basename);
+                const newFileName = getNewName(file.stem);
+                if (!newFileName) {
+                    return;
+                }
+                const newPath = path.join(baseDir, newFileName + file.extname);
+
+                fs.renameSync(oldPath, newPath); // Synchronously rename the file
+                console.log(`File renamed to ${newPath}`);
+            } catch (err) {
+                console.error(`Error renaming file: ${err}`);
+            }
+        }, 750);
+    });
+});
+
+gulp.task('default', gulp.series('watch-mp3'));
